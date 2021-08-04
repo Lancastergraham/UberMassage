be5e9c24c607df3265a12f18d7ddb09c2699c5b9
Lucas <lucasr.reehle@gmail.com>
2021-08-03 22:20:29 -0400

    Test Push

---
+++ src/main/java/Controllers/HomeController.java
@@ -0,0 +1,9 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+}
